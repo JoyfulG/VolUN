d342c953468073f231aa8fd9efa8755d0224583a
JoyfulG <joyfulbread@mail.ru>
2023-05-22 20:17:03 +0300

    Added a widget that displays bubbles with selected search parameters

---
+++ search/selected_search_params_widget.py
@@ -0,0 +1,31 @@
+from PyQt6 import QtWidgets, QtCore
+
+from search.flow_layout import FlowLayout
+from search.selected_search_param import SelectedSearchParam
+
+
+class SelectedSearchParamsWidget(QtWidgets.QScrollArea):
+    def __init__(self):
+        super(SelectedSearchParamsWidget, self).__init__()
+
+        self.setWidgetResizable(True)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setMaximumHeight(40)
+        self.setFrameShape(QtWidgets.QFrame.Shape.NoFrame)
+
+        widget = QtWidgets.QWidget()
+        self.layout = FlowLayout(widget)
+        self.setWidget(widget)
+
+    def add_item(self, item):
+        bubble = SelectedSearchParam(item)
+        self.layout.addWidget(bubble)
+
+    def remove_item(self, item):
+        index = self.layout.count()
+        while index:
+            layout_item = self.layout.itemAt(index - 1)
+            if item == layout_item.widget().item:
+                self.layout.removeWidget(layout_item.widget())
+                break
+            index -= 1
