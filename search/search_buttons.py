bce94d6d04962263b4104512ca66c105b7f140cb
JoyfulG <joyfulbread@mail.ru>
2023-06-12 19:14:58 +0300

    Clear All button is now functional

--- search/search_buttons.py
+++ search/search_buttons.py
@@ -19,3 +19,15 @@ class ClearAllButton(QtWidgets.QPushButton):
         self.setCursor(QtCore.Qt.CursorShape.PointingHandCursor)
         self.setMinimumWidth(120)
         self.setStyleSheet('QPushButton { border: none; padding: 5px; border-radius: 8px; background-color: #bcbcbc; }')
+        self.clicked.connect(self.on_clicked)
+
+    def on_clicked(self):
+        selected_options = self.parent().parent().layout().itemAt(2).widget()
+        current_quantity = selected_options.layout.count()
+        for index in reversed(range(current_quantity)):
+            layout_item = selected_options.layout.itemAt(index)
+            if type(layout_item.widget()).__name__ == 'SelectedSearchParamRanged':
+                layout_item.widget().item.input_from.setText('')
+                layout_item.widget().item.input_to.setText('')
+            elif type(layout_item.widget()).__name__ == 'SelectedSearchParamCheckbox':
+                layout_item.widget().item.setCheckState(QtCore.Qt.CheckState.Unchecked)
